a3aa70bca49821af80f6c23a5beabc64d75ae9ee
NiteshTagadghar <52933246+NiteshTagadghar@users.noreply.github.com>
2024-11-18 19:54:10 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 export const mockAuthenticate = (username, password)=> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (username === 'nitesh' && password === 'password') {
+        if (username === 'admin@gmail.com' && password === 'password') {
           resolve()
         } else {
           reject(new Error('Invalid credentials'))
